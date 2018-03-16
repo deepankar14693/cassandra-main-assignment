d828a605997e3ccbd25fa8f2e9b48e16baaca369
deepankar <deepankar.ranswal@knoldus.in>
2018-03-16 11:20:42 +0530

    assignment completed successfully

--- src/main/scala/edu/knoldus/CassandraApplication.scala
+++ src/main/scala/edu/knoldus/CassandraApplication.scala
@@ -1,40 +1,40 @@
 package edu.knoldus
 
-//import org.apache.log4j.Logger
+import org.apache.log4j.Logger
 import com.datastax.driver.core.{ConsistencyLevel, ResultSet, Session}
 
 import scala.collection.JavaConverters._
 
 object CassandraApplication extends App with CassandraSession {
- // val log = Logger.getLogger(getClass)
+  val log = Logger.getLogger(getClass)
  cassandraSession.getCluster.getConfiguration.getQueryOptions.setConsistencyLevel(ConsistencyLevel.QUORUM)
- println("\n\n*********Cluster Information *************")
- println("\n\n Cluster Name is: " +
+ log.debug("\n\n*********Cluster Information *************")
+ log.debug("\n\n Cluster Name is: " +
   cassandraSession.getCluster.getClusterName)
- println("\n\n Cluster Configuration is: " +
+ log.debug("\n\n Cluster Configuration is: " +
   cassandraSession.getCluster.getConfiguration.getQueryOptions.getConsistencyLevel)
  println("\n\n Cluster Metadata is: " +
   cassandraSession.getCluster.getMetadata.getAllHosts.toString)
 
  private def tableCreation(sessionCreate: Session): Unit = {
   println()
-  println()
-  println("*******table creation and fetching all values*********")
-  println()
+  log.debug()
+  log.debug("*******table creation and fetching all values*********")
+  log.debug()
   // sessionCreate.execute(s"create table if not exists employee(emp_id int primary key,emp_city text,emp_name text,emp_phone varint,emp_salary varint)")
   val results = sessionCreate.execute(s"create table if not exists employeesdata2(emp_id int," +
    s"emp_city text,emp_name text,emp_phone varint,emp_salary varint," +
    s"primary key(emp_id,emp_salary))")
-  println("table created successfully>>>>>>>>>>>>>>>>>>")
-  println(s"consistency level set to quorum>>>>>>>>>>>>>>")
+  log.debug("table created successfully>>>>>>>>>>>>>>>>>>")
+  log.debug(s"consistency level set to quorum>>>>>>>>>>>>>>")
 
   //  val results = sessionCreate.execute(s"select * from employee").asScala.toList
  }
 
  private def recordsInsertion(sessionCreated: Session): Unit = {
-  println()
-  println()
-  println("*******now inserting records in table emp*********")
+  log.debug()
+  log.debug()
+  log.debug("*******now inserting records in table emp*********")
   val firstRecord = sessionCreated.execute(s"insert into employeesdata2(emp_id,emp_city,emp_name,emp_phone," +
    s"emp_salary)values(1,'chandigarh','raja',78005987,40000)")
   val secondRecord = sessionCreated.execute(s"insert into employeesdata2(emp_id,emp_city,emp_name,emp_phone," +
@@ -45,30 +45,30 @@ object CassandraApplication extends App with CassandraSession {
    s"emp_salary)values(4,'pakistan','nitin',8975875987,16000)")
   val resultSet = sessionCreated.execute(s"select * from employeesdata2")
   resultSet.forEach(println(_))
-  println("records inserted>>>>>>>>>>>>>>>>")
+  log.debug("records inserted>>>>>>>>>>>>>>>>")
  }
 
  private def fetchThroughEmployeeId(sessionCreate: Session): Unit = {
-  println()
-  println()
-  println("*******fetching through particular employee id********")
+  log.debug()
+  log.debug()
+  log.debug("*******fetching through particular employee id********")
   val results = sessionCreate.execute(s"select * from employeesdata2 where emp_id = 2").asScala.toList
   results.foreach(println(_))
  }
 
  private def updateFourthRecord(sessionCreate: Session): Unit = {
-  println()
-  println()
-  println("********fourth row updated*********")
+  log.debug()
+  log.debug()
+  log.debug("********fourth row updated*********")
   val results = sessionCreate.execute(s"update employeesdata2 set  emp_city = 'chandigarh' where emp_id = 4 and emp_salary = 16000 ")
   val updatedResult = sessionCreate.execute(s"select * from employeesdata2")
   updatedResult.forEach(println(_))
  }
 
  private def salaryRecords(sessionCreated: Session): Unit = {
-  println()
-  println()
-  println("salary greater than 30000")
+  log.debug()
+  log.debug()
+  log.debug("salary greater than 30000")
   //    val query = "create index on employee(emp_salary)"
   //    val result = cassandraSession.execute(s"select emp_id,emp_city,emp_name,emp_phone,min(emp_salary) as salary from employee group by" +
   //     s"emp_id,emp_city,emp_name,emp_phone having min(emp_salary)>20000")
@@ -77,18 +77,18 @@ object CassandraApplication extends App with CassandraSession {
  }
 
  private def chandigarhDetails(sessionCreate: Session): Unit = {
-  println()
-  println()
-  println("all chandigarh residents>>>>>>>>>>>>>")
+  log.debug()
+  log.debug()
+  log.debug("all chandigarh residents>>>>>>>>>>>>>")
   //sessionCreate.execute(s"create index on employeesdata2(emp_city)")
   val result = sessionCreate.execute(s"select * from employeesdata2 where emp_city = 'chandigarh'")
   result.forEach(println(_))
  }
 
  private def deleteChandigarhDetails(sessionCreated: Session): Unit = {
-  println()
-  println()
-  println("chandigarh records deleted>>>>>>>>>>>>>>>")
+  log.debug()
+  log.debug()
+  log.debug("chandigarh records deleted>>>>>>>>>>>>>>>")
 
   sessionCreated.execute(s"create table if not exists deletechandigarh(emp_id int," +
    s"emp_city text,emp_name text,emp_phone varint,emp_salary varint," +
